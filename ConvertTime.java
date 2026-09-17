cd53b6d8b44a3648b7feff629334b2c0f9ec185a
Will Zhu <67773442+OceanOreos@users.noreply.github.com>
2026-09-17 15:00:11 -0700

    Add files via upload

---
+++ ConvertTime.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ConvertTime {
+	
+	public static void main (String[] args) {
+	
+	Scanner in = new Scanner(System.in);
+	System.out.println("Enter a number in seconds: ");
+	int seconds = in.nextInt();
+	
+	int h = seconds/3600;
+	int min = seconds%3600/60;
+	int sec = seconds%3600%60;
+	
+	
+	
+	}
+}
+	
